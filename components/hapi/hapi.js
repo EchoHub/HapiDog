cdc730d3e4a266dcd495228d0797e89c07165296
xuyang <xuyang@wdai.com>
2018-05-17 17:02:26 +0800

    update

--- components/hapi/hapi.js
+++ components/hapi/hapi.js
@@ -16,13 +16,27 @@ const _fileCopyPath = {
     switch (param[0]) {
         case "page":
             name = param[1];
-            log(param[0], name, "html");
-            copyFile(_fileCopyPath.page, path.resolve(__dirname, `./../../page/${name}.html`), "index.html", `${name}.html`)
+            log("add", param[0], name, "html", () => {
+                return copyFile(_fileCopyPath.page, path.resolve(__dirname, `./../../page/${name}.html`), "index.html", `${name}.html`)
+            });
             break;
         case "component":
             name = param[1];
-            log(param[0], name, "jsx | scss | md")
-            copyDirFiles(_fileCopyPath.component, `${dirPath.component}/${name}`, name)
+            log("add", param[0], name, "jsx | scss | md", () => {
+                return copyDirFiles(_fileCopyPath.component, `${dirPath.component}/${name}`, name)
+            })
+            break;
+        case "delete":
+            name = param[2];
+            log("remove", param[1], name, "page", () => {
+                return removeFileOrDir(_fileCopyPath.page, name, false)
+            });
+            break;
+        case "remove":
+            name = param[2];
+            log("remove", param[1], name, "component", () => {
+                return removeFileOrDir(_fileCopyPath.component, name, true)
+            });
             break;
         default:
             throw console.log("illegal param".red)
@@ -40,18 +54,38 @@ function getArgv() {
  * @param style 类型
  * @param name 模块名称
  * @param info 模版信息
+ * @param finish 完成函数
  */
-function log(type, name, info) {
+function log(behavior, type, name, info, finish) {
     const _enum = {
         "page": "页面",
         "component": "组件"
     };
     let _type = _enum[type];
-    console.log(`--- 开始创建 ---`.green);
-    console.log(`* ${_type}名称：${name}`.green);
-    console.log(`* ${_type}信息：${info}`.green);
-    console.log(`创建中，请稍等...`.green);
-    console.log(`--- 创建完成 ---`.green);
+    switch (behavior) {
+        case "add":
+            // 新增
+            console.log(`* 开始创建`.green);
+            console.log(`* ${_type}名称：${name}`.green);
+            console.log(`* ${_type}信息：${info}`.green);
+            console.log(`* 创建中，请稍等...`.green);
+            let status = true
+            finish && typeof finish === "function" && (status = finish())
+            status ? console.log(`* 创建完成`.green) : console.log("* 文件已存在，创建失败".red);
+            break;
+        case "remove":
+            // 删除
+            console.log(`* 开始删除`.green);
+            console.log(`* ${_type}名称：${name}`.green);
+            console.log(`* ${_type}信息：${info}`.green);
+            console.log(`* 删除中，请稍等...`.green);
+            let status1 = true;
+            finish && typeof finish === "function" && (status1 = finish())
+            status1 ? console.log(`* 删除完成`.green) : console.log("* 文件不存在，删除失败".red);
+            break;
+        default:
+            break;
+    }
 }
 /**
  * @desc 拷贝文件
@@ -65,12 +99,15 @@ function copyFile(src_path, dst_path, template, target) {
     let allow = true;
     files.forEach(d => {
         if (d === target) {
-            console.log("页面已存在,创建失败".red);
             allow = false;
             return;
         }
     });
-    allow && fs.createReadStream(path.resolve(src_path, template)).pipe(fs.createWriteStream(dst_path))
+    if (allow) {
+        fs.createReadStream(path.resolve(src_path, template)).pipe(fs.createWriteStream(dst_path));
+        return true;
+    }
+    return false;
 }
 /**
  * @desc 拷贝指定目录下的文件
@@ -80,15 +117,52 @@ function copyDirFiles(src, dst, target) {
     let allow = true;
     files.forEach(d => {
         if (d === target) {
-            console.log("组件已存在，创建失败");
             allow = false;
             return;
         }
     });
-    allow && fs.mkdirSync(dst);
-    fs.readdirSync(src + "/_template").forEach(d => {
-    fs.createReadStream(src + `/_template/${d}`).pipe(fs.createWriteStream(dst + `/${target}.${d.split("")[1]}`))
+    if (allow) {
+        fs.mkdirSync(dst);
+        fs.readdir(src + "/_template", null, (err, files) => {
+            files.forEach(d => {
+                fs.createReadStream(src + `/_template/${d}`).pipe(fs.createWriteStream(dst + `/${target}.${d.split("")[1]}`))
+            })
+        })
+        return true;
+    }
+    return false;
+}
+
+/**
+ * @desc 删除指定目录或文件
+ * @param -v 删除页面 view
+ * @param -c 删除组件 component
+ */
+function removeFileOrDir(path, filename, fileOrDir) {
+    let allow = false;
+    fs.readdirSync(path).forEach(file => {
+        if(file.split(".")[0] === filename) {
+            allow = true;
+            const _path = `${path}/${filename}`;
+            if(fileOrDir) {
+                removeFiles(_path)
+                fs.rmdirSync(_path)
+            }else {
+                fs.unlink(`${_path}.${file.split(".")[1]}`)
+            }
+            // fileOrDir ? removeFiles(_path) && fs.rmdirSync(_path) : fs.unlink(`${_path}.${file.split(".")[1]}`)
+            return;
+        }
+    });
+    if(allow){
+        return true;
+    }
+    return false;
+}
+
+function removeFiles(path) {
+    fs.readdirSync(path).forEach(file => {
+        const _path = `${path}/${file}`;
+        fs.statSync(_path).isDirectory() ? removeFiles(_path) : fs.unlinkSync(_path)
     })
-
-
 }
---
+++ components/test/test.t
@@ -0,0 +1,5 @@
+._templage{
+    
+}ass Template {
+    
+}
